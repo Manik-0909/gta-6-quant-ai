595db7984f2ad0f9be6f2b4be372572081ef685b
Manik Gupta <manikgupta254@gmail.com>
2025-06-05 21:54:51 +0530

    Added completed code

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Page Loader Animation
+window.addEventListener("DOMContentLoaded", function () {
+   const loader = document.getElementById("page-loader");
+
+   setTimeout(() => {
+      loader.classList.add("fade-out");
+
+      setTimeout(() => loader.remove(), 100);
+   }, 1000);
+});
+
+// Header Background Change After Scroll
+window.addEventListener("scroll", function () {
+   const header = document.getElementById("mainHeader");
+   if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+   } else {
+      header.classList.remove("scrolled");
+   }
+});
